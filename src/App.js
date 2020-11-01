5065d928a17dbc1a814762070a6932e2ec104d76
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-01 19:32:27 +0000

    Added list for table

--- src/App.js
+++ src/App.js
@@ -32,6 +32,37 @@ function App() {
       {
         err && <><hr className="mt-2 mb-2" /><div className="alert-danger" ><h5>check out your request, a city and category must be chosen, No default values are provided</h5></div></>
       }
+      
+      <hr />
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <button className={isVisible.pharmacies ? "nav-link active" : "nav-link"} onClick={(e) => { grapPharmacies(); handleTabClick(e) }} value="pharmacies" >Pharmacies</button>
+        </li>
+        {/* <li className="nav-item">
+          <button className={isVisible.highSchools ? "nav-link active" : "nav-link"} onClick={(e) => { handleTabClick(e) }} value="highSchools" >high schools</button>
+        </li> */}
+        <li className="nav-item">
+          <button className={isVisible.colleges ? "nav-link active" : "nav-link"} onClick={(e) => { grapColleges(); handleTabClick(e) }} value="colleges" > schools & colleges</button>
+        </li>
+        <li className="nav-item">
+          <button className={isVisible.hospitals ? "nav-link active" : "nav-link"} onClick={(e) => { grapHospitals(); handleTabClick(e) }} value="hospitals" >hospitals</button>
+        </li>
+        <li className="nav-item">
+          <button className={isVisible.Gps ? "nav-link active" : "nav-link"} onClick={(e) => { grapDoctors(); handleTabClick(e) }} value="Gps" >doctors</button>
+        </li>
+      </ul>
+
+
+
+      {/* <hr />
+      <div>
+        <span>show: {' '} {' '} {' '} {' '}</span>
+        <div style={{ display: 'inline' }} class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="defaultChecked2" checked />
+          <label class="custom-control-label" for="defaultChecked2">Default checked</label>
+        </div>
+      </div> */}
+      <br className="mb-5" />
     </div>
   );
 }
